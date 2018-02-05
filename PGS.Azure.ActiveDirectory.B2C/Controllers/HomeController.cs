85246bf47b60c71060236ccae571028f3475e268
Tomasz Kielar <tkielar@pgs-soft.com>
2018-02-05 14:49:26 +0100

    Empty app

---
+++ PGS.Azure.ActiveDirectory.B2C/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PGS.Azure.ActiveDirectory.B2C.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
