ef5679d3e51eda2350a890c607fd8ecd43c84c67
Tomasz Kielar <tkielar@pgs-soft.com>
2018-02-07 09:55:19 +0100

    Azure AD settings

---
+++ PGS.Azure.ActiveDirectory.B2C/Auth/AzureAdOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace PGS.Azure.ActiveDirectory.B2C.Auth
+{
+    public class AzureAdOptions
+    {
+        public string ClientId { get; set; }
+        public string Tenant { get; set; }
+        public string CallbackPath { get; set; }
+        public string DefaultPolicy { get; set; }
+    }
+}
